0e6b7160096e91ea7b2e3371ce0161daddad6d36
hawaiikaiboy <42709206+hawaiikaiboy@users.noreply.github.com>
2021-11-06 14:54:26 -1000

    FRC 2022 Swerve drive start

---
+++ SwerveBase2022/src/main/java/frc/robot/OI.java
@@ -0,0 +1,8 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.Joystick;
+
+//import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+public class OI {
+    public Joystick Driver = new Joystick(0);
+    
+}
