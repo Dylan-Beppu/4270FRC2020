a5839418388afbb4b94f14cc2d6710ed0bdfd355
hawaiikaiboy <42709206+hawaiikaiboy@users.noreply.github.com>
2021-11-10 14:49:03 -1000

    Made a copy and using frc 2910 code

---
+++ SwerveBaseFRCdefalut/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,164 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+//import com.revrobotics.jni.CANSparkMaxJNI;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  //TODO: prefferbaly mount on RoboRio
+  public static AHRS gyro = new AHRS(Port.kUSB);
+
+  public static final class OIConstants {
+    //Note Opperator inputs are now in the constants file
+    //TODO: add the xbox and switch configs (and maby ps5?)
+    public static final Joystick Driver = new Joystick(0);
+    
+    public static final JoystickButton DriBtn1 = new JoystickButton(Driver, 1);
+    public static final JoystickButton DriBtn2 = new JoystickButton(Driver, 2);
+    public static final JoystickButton DriBtn3 = new JoystickButton(Driver, 3);
+    public static final JoystickButton DriBtn4 = new JoystickButton(Driver, 4);
+    public static final JoystickButton DriBtn5 = new JoystickButton(Driver, 5);
+    public static final JoystickButton DriBtn6 = new JoystickButton(Driver, 6);
+    public static final JoystickButton DriBtn7 = new JoystickButton(Driver, 7);
+    public static final JoystickButton DriBtn8 = new JoystickButton(Driver, 8);
+    public static final JoystickButton DriBtn9 = new JoystickButton(Driver, 9);
+    public static final JoystickButton DriBtn10 = new JoystickButton(Driver, 10);
+    
+     
+     
+     //public Joystick BtnPanle = new Joystick(1);
+  }
+
+  public static final class DriveConstants {
+    //public static final TalonSRX kRearLeftDriveMotor = new TalonSRX(2);
+    public static final CANSparkMax kFrontLeftDriveMotor = new CANSparkMax(1, MotorType.kBrushless);
+    public static final CANSparkMax kRearLeftDriveMotor = new CANSparkMax(2, MotorType.kBrushless);
+    public static final CANSparkMax kFrontRightDriveMotor = new CANSparkMax(3, MotorType.kBrushless);
+    public static final CANSparkMax kRearRightDriveMotor = new CANSparkMax(4, MotorType.kBrushless);
+    public static final CANSparkMax kFrontLeftTurningMotor = new CANSparkMax(5, MotorType.kBrushless);
+    public static final CANSparkMax kRearLeftTurningMotor = new CANSparkMax(6, MotorType.kBrushless);
+    public static final CANSparkMax kFrontRightTurningMotor = new CANSparkMax(7, MotorType.kBrushless);
+    public static final CANSparkMax kRearRightTurningMotor = new CANSparkMax(8, MotorType.kBrushless);
+
+
+    //TODO: rename for canIDs actuly redo theis whole
+    // public static final int kFrontLeftDriveMotorPort = 0;
+    // public static final int kRearLeftDriveMotorPort = 2;
+    // public static final int kFrontRightDriveMotorPort = 4;
+    // public static final int kRearRightDriveMotorPort = 6;
+
+    // public static final int kFrontLeftTurningMotorPort = 1;
+    // public static final int kRearLeftTurningMotorPort = 3;
+    // public static final int kFrontRightTurningMotorPort = 5;
+    // public static final int kRearRightTurningMotorPort = 7;
+    //TODO: figure out the encoder stuff and find out if the ones on the mk2 modules can substitute in
+    //TODO: also see if the encoders can be swaped for the intergrated ones (so redundent other one???)
+    // public static final int[] kFrontLeftTurningEncoderPorts = new int[] {0, 1};
+    // public static final int[] kRearLeftTurningEncoderPorts = new int[] {2, 3};
+    // public static final int[] kFrontRightTurningEncoderPorts = new int[] {4, 5};
+    // public static final int[] kRearRightTurningEncoderPorts = new int[] {5, 6};
+
+    public static final boolean kFrontLeftTurningEncoderReversed = false;
+    public static final boolean kRearLeftTurningEncoderReversed = true;
+    public static final boolean kFrontRightTurningEncoderReversed = false;
+    public static final boolean kRearRightTurningEncoderReversed = true;
+
+    // public static final int[] kFrontLeftDriveEncoderPorts = new int[] {7, 8};
+    // public static final int[] kRearLeftDriveEncoderPorts = new int[] {9, 10};
+    // public static final int[] kFrontRightDriveEncoderPorts = new int[] {11, 12};
+    // public static final int[] kRearRightDriveEncoderPorts = new int[] {13, 14};
+
+    public static final boolean kFrontLeftDriveEncoderReversed = false;
+    public static final boolean kRearLeftDriveEncoderReversed = true;
+    public static final boolean kFrontRightDriveEncoderReversed = false;
+    public static final boolean kRearRightDriveEncoderReversed = true;
+
+    public static final double kTrackWidth = 0.5;
+    // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = 0.7;
+    // Distance between front and back wheels on robot
+    public static final SwerveDriveKinematics kDriveKinematics =
+        new SwerveDriveKinematics(
+            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    public static final boolean kGyroReversed = false;
+
+    // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
+    // These characterization values MUST be determined either experimentally or theoretically
+    // for *your* robot's drive.
+    // The RobotPy Characterization Toolsuite provides a convenient tool for obtaining these
+    // values for your robot.
+    public static final double ksVolts = 1;
+    public static final double kvVoltSecondsPerMeter = 0.8;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.15;
+
+    public static final double kMaxSpeedMetersPerSecond = 3;
+  }
+
+  public static final class ModuleConstants {
+    public static final double kMaxModuleAngularSpeedRadiansPerSecond = 2 * Math.PI;
+    public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 2 * Math.PI;
+
+    public static final int DriveEncoderCPR = 1024;
+    public static final double DriveWheelDiameterMeters = 0.15;
+    public static final double DriveEncoderDistancePerPulse =
+        // Assumes the encoders are directly mounted on the wheel shafts
+        (DriveWheelDiameterMeters * Math.PI) / (double) DriveEncoderCPR;
+
+    public static final double DriveTurningEncoderDistancePerPulse =
+        // Assumes the encoders are on a 1:1 reduction with the module shaft.
+        (2 * Math.PI) / (double) DriveEncoderCPR;
+
+    public static final double DrivePModuleTurningController = 1;
+
+    public static final double kPModuleDriveController = 1;
+  }
+
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+    public static final double kPXController = 1;
+    public static final double kPYController = 1;
+    public static final double kPThetaController = 1;
+
+    // Constraint for the motion profilied robot angle controller
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+        new TrapezoidProfile.Constraints(
+            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  }
+}
---
+++ SwerveBaseFRCdefalut/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,125 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import frc.robot.Constants.ModuleConstants;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class SwerveModule {
+  private final CANSparkMax m_driveMotor;
+  private final CANSparkMax m_turningMotor;
+
+  //private final Encoder m_driveEncoder;
+  //private final Encoder m_turningEncoder;
+
+  private final PIDController m_drivePIDController =
+      new PIDController(ModuleConstants.kPModuleDriveController, 0, 0);
+
+  // Using a TrapezoidProfile PIDController to allow for smooth turning
+  private final ProfiledPIDController m_turningPIDController =
+      new ProfiledPIDController(
+          ModuleConstants.DrivePModuleTurningController,
+          0,
+          0,
+          new TrapezoidProfile.Constraints(
+              ModuleConstants.kMaxModuleAngularSpeedRadiansPerSecond,
+              ModuleConstants.kMaxModuleAngularAccelerationRadiansPerSecondSquared));
+
+  /**
+   * Constructs a SwerveModule.
+   *
+   * @param driveMotorChannel ID for the drive motor.
+   * @param turningMotorChannel ID for the turning motor.
+   */
+  public SwerveModule(
+      CANSparkMax driveMotorChannel,
+      CANSparkMax turningMotorChannel,
+      boolean driveEncoderReversed,
+      boolean turningEncoderReversed) {
+
+    m_driveMotor = driveMotorChannel;
+    m_turningMotor = turningMotorChannel;
+
+
+    //this.m_driveEncoder = m_driveMotor.getEncoder();
+    //this.m_driveEncoder = new Encoder(driveEncoderPorts[0], driveEncoderPorts[1]);
+    //this.m_turningEncoder = m_turningMotor.getEncoder();
+    //this.m_turningEncoder = new Encoder(turningEncoderPorts[0], turningEncoderPorts[1]);
+
+    // Set the distance per pulse for the drive encoder. We can simply use the
+    // distance traveled for one rotation of the wheel divided by the encoder
+    // resolution.
+    //TODO:set the right cpm in constancs
+    //m_driveMotor  //setDistancePerPulse(ModuleConstants.kDriveEncoderDistancePerPulse);
+      
+    // Set whether drive encoder should be reversed or not
+  //  m_driveEncoder.setReverseDirection(driveEncoderReversed);
+//_driveMotor.encoder
+    // Set the distance (in this case, angle) per pulse for the turning encoder.
+    // This is the the angle through an entire rotation (2 * wpi::math::pi)
+    // divided by the encoder resolution.
+    //m_turningEncoder.setDistancePerPulse(ModuleConstants.kTurningEncoderDistancePerPulse);
+
+    // Set whether turning encoder should be reversed or not
+   // m_turningEncoder.setReverseDirection(turningEncoderReversed);
+
+    // Limit the PID Controller's input range between -pi and pi and set the input
+    // to be continuous.
+    m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+  }
+
+  /**
+   * Returns the current state of the module.
+   *
+   * @return The current state of the module.
+   */
+  public SwerveModuleState getState() {
+    //SwerveModuleState()
+    //return new SwerveModuleState(m_driveEncoder.getRate(), new Rotation2d(m_turningEncoder.get()));
+    return new SwerveModuleState(m_driveMotor.getEncoder().getVelocity(), new Rotation2d(m_turningMotor.getEncoder().getPosition()));
+
+  }
+
+  /**
+   * Sets the desired state for the module.
+   *
+   * @param desiredState Desired state with speed and angle.
+   */
+  public void setDesiredState(SwerveModuleState desiredState) {
+    // Optimize the reference state to avoid spinning further than 90 degrees
+    SwerveModuleState state =
+        //SwerveModuleState.optimize(desiredState, new Rotation2d(m_turningEncoder.get()));
+        SwerveModuleState.optimize(desiredState, new Rotation2d(m_turningMotor.getEncoder().getCountsPerRevolution()));
+
+    // Calculate the drive output from the drive PID controller.
+    final double driveOutput =
+        //m_drivePIDController.calculate(m_driveEncoder.getRate(), state.speedMetersPerSecond);
+        m_drivePIDController.calculate(m_driveMotor.getEncoder().getVelocity(), state.speedMetersPerSecond);
+
+    // Calculate the turning motor output from the turning PID controller.
+    final double turnOutput =
+        //m_turningPIDController.calculate(m_turningEncoder.get(), state.angle.getRadians());
+        m_turningPIDController.calculate(m_turningMotor.getEncoder().getPosition(), state.angle.getRadians());
+
+    // Calculate the turning motor output from the turning PID controller.
+    m_driveMotor.set(driveOutput);
+    m_turningMotor.set(turnOutput);
+  }
+
+  /** Zeros all the SwerveModule encoders. */
+  public void resetEncoders() {
+    m_driveMotor.getEncoder().setPosition(0);
+    m_turningMotor.getEncoder().setPosition(0);
+  }
+}
